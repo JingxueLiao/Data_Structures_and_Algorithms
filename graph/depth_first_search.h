755950cbcd5bbeb1646d666762cdad415eb273ca
JingxueLiao <jingxue.liao@gmail.com>
2016-07-06 09:50:11 +0800

    Update depth_first_search

--- graph/depth_first_search.h
+++ graph/depth_first_search.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <vector>
+#include <stack>
 
 using std::vector;
+using std::stack;
 
 void depth_first_search_helper(const vector<vector<int>> &graph, vector<char> &colors, int cur) {
     colors[cur] = 'g';
@@ -19,6 +21,32 @@ void depth_first_search(const vector<vector<int>> &graph) {
     for (int i = 0; i < graph.size(); ++i) {
         if (colors[i] == 'w') {
             depth_first_search_helper(graph, colors, i);
+        }
+    }
+}
+
+void depth_first_search_iterator(const vector<vector<int>> &graph) {
+    vector<char> colors(graph.size(), 'w');
+    stack<int> path;
+    for (int i = 0; i < graph.size(); ++i) {
+        if (colors[i] == 'w') {
+            path.push(i);
+            while (!path.empty()) {
+                int cur = path.top();
+                if (colors[cur] == 'w') {
+                    colors[cur] = 'g';
+                    for (auto p = graph[cur].begin(); p != graph[cur].end(); ++p) {
+                        if (colors[*p] == 'w') {
+                            path.push(*p);
+                        }
+                    }
+                } else if (colors[cur] == 'g') {
+                    colors[cur] = 'b';
+                    path.pop();
+                } else if (colors[cur] == 'b') {
+                    path.pop();
+                }
+            }
         }
     }
 }
